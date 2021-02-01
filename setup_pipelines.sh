684c311be093d35846a09e01e4f9672782a00677
Mike LaCourse <mlacours@unknown60f262ee1fce.attlocal.net>
2021-02-01 16:15:28 -0500

    remove login

--- setup_pipelines.sh
+++ setup_pipelines.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 echo "Installing Pipeline" 
-#export KUBECONFIG=../ocp-labs-manager/config/ocp-demo-cluster/ocp-install/auth/kubeconfig
-echo "Cluster URL: $CLUSTER_URL"
-oc login $OCP_CLUSTER -u $OC_USERNAME -p $OC_PASSWORD
 oc project cicd-tools
 
 git checkout stage
