14ee1ddec60785521bfceb2a8966a1c6f6e0d5f1
Mike LaCourse <mlacours@localhost.localdomain>
2021-02-04 11:14:37 -0500

    updates

--- setup_pipelines.sh
+++ setup_pipelines.sh
@@ -2,7 +2,7 @@
 echo "Installing Pipeline" 
 oc project cicd-tools
 
-git checkout stage
+git checkout main
 helm upgrade --install spring-demo-stage ./helm/argo-app/ --values ./helm/argo-app/stage-values.yaml --set googlechat_secret=foobar
 
 #git checkout qa
