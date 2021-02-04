7d34d460b6956e97c206cf0631e8ce6631b8dd78
Mike LaCourse <mlacours@localhost.localdomain>
2021-02-04 11:12:52 -0500

    helm/

--- setup_pipelines.sh
+++ setup_pipelines.sh
@@ -3,7 +3,7 @@ echo "Installing Pipeline"
 oc project cicd-tools
 
 git checkout stage
-oc apply -f .openshift/spring-boot-demo.yaml
+helm upgrade --install spring-demo-stage ./helm/argo-app/ --values ./helm/argo-app/stage-values.yaml --set googlechat_secret=foobar
 
-git checkout qa
-oc apply -f .openshift/spring-boot-demo.yaml
+#git checkout qa
+#oc apply -f .openshift/spring-boot-demo.yaml
