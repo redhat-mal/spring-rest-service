7ce600411f023aa205e3aac1307e5016c6b1cb90
Mike LaCourse <mlacours@localhost.localdomain>
2021-02-05 14:21:02 -0500

    fix

--- setup_pipelines.sh
+++ setup_pipelines.sh
@@ -5,7 +5,7 @@ oc project cicd-tools
 git checkout stage
 if [[ -z "$MY_GOOGLECHAT_SECRET" ]]; then
 echo "Chat Disabled"
-helm upgrade --install spring-rest-demo-stage ./helm/argo-app/ -set googlechat_secret=false 
+helm upgrade --install spring-rest-demo-stage ./helm/argo-app/ --set googlechat_secret=false 
 else
 echo "Chat Enabled"
 helm upgrade --install spring-rest-demo-stage ./helm/argo-app/ --set googlechat_secret=$MY_GOOGLECHAT_SECRET --set googlechat_channel=$MY_GOOGLECHAT_CHANNEL --set googlechat_token=$MY_GOOGLECHAT_TOKEN --set googlechat_key=$MY_GOOGLECHAT_KEY
