19516aa2dccc68d5abcedbee00ece69d89fa714e
Mike LaCourse <mlacours@localhost.localdomain>
2021-02-05 12:03:54 -0500

    add chat

--- setup_pipelines.sh
+++ setup_pipelines.sh
@@ -3,7 +3,13 @@ echo "Installing Pipeline"
 oc project cicd-tools
 
 git checkout stage
-helm upgrade --install spring-rest-demo-stage ./helm/argo-app/ --set googlechat_secret=foobar
+if [[ -z "$MY_GOOGLECHAT_SECRET" ]]; then
+echo "Chat Disabled"
+helm upgrade --install spring-rest-demo-stage ./helm/argo-app/ 
+else
+echo "Chat Enabled"
+helm upgrade --install spring-rest-demo-stage ./helm/argo-app/ --set googlechat_secret=$MY_GOOGLECHAT_SECRET --set googlechat_channel=$ MY_GOOGLECHAT_CHANNEL --set googlechat_token=$MY_GOOGLECHAT_TOKEN --set googlechat_key=$MY_GOOGLECHAT_KEY
+fi
 
 git checkout qa
 helm upgrade --install spring-rest-demo-qa ./helm/argo-app/  --set googlechat_secret=foobar
